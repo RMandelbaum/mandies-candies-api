f41c32d5f011d4d218fdd7ddb379376cd85b4104
Rachel Mandelbaum <rlmandelbaum@gmail.com>
2018-01-21 20:57:31 -0500

    added seed data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+ chocolates = Chocolate.create([
+   {
+     chocolate_type: "Milk Chocolate",
+     price: 3,
+     piece_count: 15,
+     img_url: "http://images.godiva.com/is/image/godiva/22pc-milk-chocolate-gift-box~~190903-2?$plp$",
+     gift_wrapped: true
+   },
+   {
+     chocolate_type: "Dark Chocolate",
+     price: 4,
+     piece_count: 15,
+     img_url: "http://images.godiva.com/is/image/godiva/24pc-dark-chocolate-truffles~~78341-2?$plp$",
+     gift_wrapped: false
+   },
+
+   {
+     chocolate_type: "White Chocolate",
+     price: 3,
+     piece_count: 15,
+     img_url: "http://images.godiva.com/is/image/godiva/24pc-white-chocolate-gift-box~~190442-2?$plp$",
+     gift_wrapped: false
+   },
+   {
+     chocolate_type: "Assorted Chocolate",
+     price: 3,
+     piece_count: 15,
+     img_url: "http://images.godiva.com/is/image/godiva/12pc-signature-chocolate-truffles-gift-box-classic~~78331-2?$plp$",
+     gift_wrapped: false
+   }
+ ])
